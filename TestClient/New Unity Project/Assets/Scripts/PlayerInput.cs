6aeac0cf6d8929eb9d4002f60589ae2b3b0978af
Chaeyoung2 <xxll0331@hanmail.net>
2020-03-03 22:45:42 +0900

    upload testing client

---
+++ TestClient/New Unity Project/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public string moveAxisName = "Vertical"; // 앞뒤 움직임 입력축
+    public string rotateAxisName = "Horizontal"; // 좌우 회전 입력축
+
+    public float move { get; private set; } // 감지된 움직임 입력 값
+    public float rotate { get; private set; } // 감지된 회전 입력 값
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 로컬 플레이어가 아닌 경우 입력을 받지 않는다.
+        //if ()
+        //{
+        //    return;
+        //}
+
+        // 게임 오버인 경우 사용자 입력을 감지하지 않는다.
+        //if ()
+        //{
+        //    return;
+        //}
+
+        // move에 관한 입력 감지
+        move = Input.GetAxis(moveAxisName);
+        rotate = Input.GetAxis(rotateAxisName);
+        
+    }
+}
---
+++ TestClient/New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; // 앞뒤 움직임 속도
+    public float rotateSpeed = 180f; // 좌우 회전 속도
+
+    private PlayerInput playerInput;
+    private Rigidbody playerRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
+    private void FixedUpdate()
+    {
+        // 로컬 플레이어만 위치와 회전 변경 가능 
+        //if ()
+        //{
+        //}
+
+        Rotate();
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 moveDistance = playerInput.move * transform.forward * moveSpeed * Time.deltaTime;
+        playerRigidbody.MovePosition(playerRigidbody.position + moveDistance);
+    }
+
+    private void Rotate()
+    {
+        float turn = playerInput.rotate * rotateSpeed * Time.deltaTime;
+        playerRigidbody.rotation = playerRigidbody.rotation * Quaternion.Euler(0, turn, 0f);
+    }
+}
